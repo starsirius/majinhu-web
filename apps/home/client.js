367e5b499d575e8b128598e12d32d189441412e4
Chung-Yi Chi <cychi1210@gmail.com>
2014-03-06 21:57:32 -0500

    Set up templates.

--- apps/home/client.js
+++ apps/home/client.js
@@ -9,38 +9,18 @@
 // composable in general.
 //
 
-var Backbone = require('backbone'),
-    $ = require('jquery'),
-    sd = require('sharify').data,
-    Commits = require('../../collections/commits.js'),
-    listTemplate = function() {
-      return require('./templates/list.jade').apply(null, arguments)
-    };
+var Backbone = require('backbone')
+  , $ = require('jquery')
+  , sd = require('sharify').data;
+
 Backbone.$ = $;
 
-module.exports.CommitsView = CommitsView = Backbone.View.extend({
+module.exports.HomeView = HomeView = Backbone.View.extend({
 
-  initialize: function() {
-    this.collection.on('sync', this.render, this);
-  },
-
-  render: function() {
-    this.$('#commits-list').html(listTemplate({ commits: this.collection.models }));
-  },
-
-  events: {
-    'change .search-input': 'changeRepo'
-  },
-
-  changeRepo: function(e) {
-    this.collection.repo = $(e.target).val();
-    this.collection.fetch();
-  }
 });
 
 module.exports.init = function() {
   new CommitsView({
-    el: $('body'),
-    collection: new Commits(sd.COMMITS, { owner: 'artsy', repo: 'flare' })
+    el: $('body')
   });
 };
