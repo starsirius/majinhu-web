54a62caa13b0a9b4ee48c575cb2333ecf26e8116
Chung-Yi Chi <cychi1210@gmail.com>
2014-03-12 02:08:08 -0400

    Use Backbone's events in views.

--- apps/admin/client.js
+++ apps/admin/client.js
@@ -18,15 +18,17 @@ Backbone.$ = $;
 
 module.exports.AdminView = AdminView = Backbone.View.extend({
 
-  initialize: function() {
-    $('#artwork-create-form').on('submit', this.submitForm);
+  events: {
+    'submit #artwork-create-form': 'submitForm'
   },
+
+  initialize: function() {},
 
   submitForm: function(e) {
     e.preventDefault();
 
     var data = $(e.currentTarget).serializeObject()
-      , artwork = new Artwork(data);
+      , artwork = new Artwork(this.transformFormData(data));
 
     artwork.url = sd.API_URL + "/artworks";
     artwork.save(null, {
@@ -38,13 +40,27 @@ module.exports.AdminView = AdminView = Backbone.View.extend({
             );
           }
         } else {
-
         }
       },
       error: function(model, response, options) {
       }
     });
+  },
+
+  transformFormData: function(data) {
+    var map = {
+      width     : function(v) { return parseFloat(v); },
+      height    : function(v) { return parseFloat(v); },
+      category  : function(v) { return [v]; },
+      date      : function(v) { return new Date(v).toUTCString(); }
+    }
+
+    for (var k in data) {
+      if (k in map) data[k] = map[k](data[k]);
+    }
+    return data;
   }
+
 });
 
 module.exports.init = function() {
