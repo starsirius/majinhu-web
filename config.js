c8ad3a1a894be8c60e9a509c25a53f03d06c7942
Chung-Yi Chi <cychi1210@gmail.com>
2014-03-22 23:51:21 -0400

    Token auth for regular api endpoints.

--- config.js
+++ config.js
@@ -6,9 +6,11 @@
 //
 
 module.exports = {
-  NODE_ENV: 'development',
-  PORT: 4000,
-  API_URL: 'http://127.0.0.1:5000'
+  NODE_ENV    : 'development',
+  PORT        : 4000,
+  API_URL     : 'http://127.0.0.1:5000',
+  APP_ID      : 'D336FF2DDAD57', // 128-bit key
+  APP_SECRET  : '0aop8udOJuWLBY5oLMvvBVhQPZYWz9BV' // 256-bit key
 }
 
 // Override any values with env variables
