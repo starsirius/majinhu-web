b750dd0b429d92eee4bae520d5cbe8020d81c722
Chung-Yi Chi <cychi1210@gmail.com>
2014-03-24 01:16:18 -0400

    Use dynamic data in artworks page.

--- apps/artworks/routes.js
+++ apps/artworks/routes.js
@@ -1,8 +1,18 @@
 // 
 // Routes file that exports route handlers for ease of testing.
 // 
+var sd = require('sharify').data
+  , Artworks = require('../../collections/artworks');
 
 exports.index = function(req, res, next) {
-  res.locals.sd.DATA = {};
-  res.render('index', {});
+  var artworks = new Artworks();
+
+  artworks.fetch({
+    url: sd.API_URL + '/artists/jinhu-ma/artworks',
+    success: function() {
+      res.locals.sd.ARTWORKS = artworks.toJSON();
+      res.render('index', {artworks: artworks.models});
+    },
+    error: function(m, err) { next(err.text); }
+  })
 };
