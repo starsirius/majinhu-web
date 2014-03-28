b6c600c4ea68ee04a4440cc10fc3443fb93ad395
Chung-Yi Chi <cychi1210@gmail.com>
2014-03-28 00:27:28 -0400

    Social sharing.

--- config.js
+++ config.js
@@ -8,6 +8,7 @@
 module.exports = {
   NODE_ENV    : 'development',
   PORT        : 4000,
+  APP_URL     : 'http://127.0.0.1:4000',
   API_URL     : 'http://127.0.0.1:5000',
   APP_ID      : 'D336FF2DDAD57', // 128-bit key
   APP_SECRET  : '0aop8udOJuWLBY5oLMvvBVhQPZYWz9BV' // 256-bit key
