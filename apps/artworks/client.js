7ce055ea78702b29f51feffc491c698f7a20edb1
Chung-Yi Chi <cychi1210@gmail.com>
2014-04-13 01:12:48 -0400

    Use BlackInkGallery lib on github. Add artworks client.js.

--- apps/artworks/client.js
+++ apps/artworks/client.js
@@ -11,12 +11,80 @@
 
 var Backbone = require('backbone')
   , $ = require('jquery')
-  , sd = require('sharify').data;
+  , sd = require('sharify').data
+  , BlackInkGallery = require('black-ink-gallery');
 
 Backbone.$ = $;
 
 module.exports.ArtworksView = ArtworksView = Backbone.View.extend({
 
+  events: {
+    "mouseover .scroll-button-left, .scroll-button-right" : "scrolling",
+    "mouseout .scroll-button-left, .scroll-button-right"  : "stopScrolling"
+  },
+
+  initialize: function() {
+    this.setupContainer(); // need to set the container's height before initializing blackInkGallery
+    this.$('#gallery').blackInkGallery();
+  },
+
+  render: function() {
+  },
+
+  setupContainer: function() {
+    var $container = $('#artworks > .container');
+
+    if ($('body')[0].style["-webkit-writing-mode"] != undefined) {
+      // $.height() set the content's height regardless of box-sizing
+      // so we have to set the height before adding the class.
+      $container.height(this.getWindowHeight())
+        .addClass('vertical-rl');
+    }
+  },
+
+  scrolling: function(e) {
+    var interval = 10
+      , velocity = 10
+      , $button  = $(e.currentTarget);
+
+    if (this.scrollingId != null) return;
+
+    this.scrollingId = setInterval(function() {
+      left = $('.vertical-rl').scrollLeft();
+
+      if ($button.hasClass("scroll-button-left")) {
+        $('.vertical-rl').scrollLeft(left - velocity);
+      }
+      if ($button.hasClass("scroll-button-right")) {
+        $('.vertical-rl').scrollLeft(left + velocity);
+      }
+    }, interval);
+  },
+
+  stopScrolling: function(e) {
+    if (this.scrollingId != null) {
+      clearInterval(this.scrollingId);
+      this.scrollingId = null;
+    }
+  },
+
+  // Re: http://stackoverflow.com/questions/8205812/jquery-js-ios-4-and-document-height-problems
+  getWindowHeight: function() {
+    // Get zoom level of mobile Safari
+    // Note, that such zoom detection might not work correctly in other browsers
+    // We use width, instead of height, because there are no vertical toolbars :)
+    var zoomLevel = document.documentElement.clientWidth / window.innerWidth;
+
+    // window.innerHeight returns height of the visible area.
+    // We multiply it by zoom and get out real height.
+    return window.innerHeight * zoomLevel;
+  },
+
+    // You can also get height of the toolbars that are currently displayed
+  getHeightOfIOSToolbars: function() {
+    var tH = (window.orientation === 0 ? screen.height : screen.width) -  getWindowHeight();
+    return tH > 1 ? tH : 0;
+  }
 });
 
 module.exports.init = function() {
--- assets/js/artworks.js
+++ assets/js/artworks.js
@@ -1,9 +1,9 @@
 //
-// The javscript asset package for the "artwork" app.
+// The javscript asset package for the "artworks" app.
 //
 // It's a good pattern to organize your asset packages by a package per app.
 // This generally means these javascript asset files are going to be quite
 // small, often just a line of initialize code like this.
 //
 
-require('jquery')(require('../../apps/artwork/client.js').init)
+require('jquery')(require('../../apps/artworks/client.js').init)
